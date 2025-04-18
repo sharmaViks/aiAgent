cd738638df9c163a04c1c22b7b12da33a1b92973
sharmaViks <vikrantsharma725@gmail.com>
2025-04-18 18:16:22 +0530

    js executor tool

--- server/agent/agent.js
+++ server/agent/agent.js
@@ -1,20 +1,60 @@
-import {createReactAgent} from "@langchain/langgraph/prebuilt";
-import {ChatAnthropic} from "@langchain/anthropic";
-import {z} from "zod";
+import { createReactAgent } from "@langchain/langgraph/prebuilt";
+import { ChatAnthropic } from "@langchain/anthropic";
+import { z } from "zod";
 import { tool } from "@langchain/core/tools";
 import { MemorySaver } from "@langchain/langgraph";
 
-const weatherTool = tool(async ({query}) => {
+const weatherTool = tool(async ({ query }) => {
     console.log('query', query);
     return 'Weather is sunny';
 },
-{
-    name: "weather",
-    description: "Get the weather in a specific location",
-    schema: z.object({
-        query: z.string().describe("The query to use in search"),
-    }),
-});
+    {
+        name: "weather",
+        description: "Get the weather in a specific location",
+        schema: z.object({
+            query: z.string().describe("The query to use in search"),
+        }),
+    });
+
+async function evalAndCaptureOutput(code) {
+    const oldLog = console.log;
+    const oldError = console.error;
+
+    const output = [];
+    let errorOutput = [];
+
+    console.log = (...args) => output.push(args.join(' '));
+    console.error = (...args) => errorOutput.push(args.join(' '));
+
+    try {
+        await eval(code);
+    } catch (error) {
+        errorOutput.push(error.message);
+    }
+
+    console.log = oldLog;
+    console.error = oldError;
+
+    return { stdout: output.join('\n'), stderr: errorOutput.join('\n') };
+}
+
+
+const jsExecutorTool = tool(async ({ code }) => {
+    const result = await evalAndCaptureOutput(code);
+    return result;
+},
+    {
+        name: 'run_javascript_code_tool',
+        description: `
+      Run general purpose javascript code. 
+      This can be used to access Internet or do any computation that you need. 
+      The output will be composed of the stdout and stderr. 
+      The code should be written in a way that it can be executed with javascript eval in node environment.
+    `,
+        schema: z.object({
+            code: z.string().describe('code to be executed'),
+        }),
+    })
 
 const model = new ChatAnthropic({
     model: "claude-3-5-sonnet-latest",
@@ -25,8 +65,8 @@ const memorySaver = new MemorySaver();
 
 export const agent = createReactAgent({
     llm: model,
-    tools: [weatherTool],
-    checkpointSaver: memorySaver 
+    tools: [weatherTool, jsExecutorTool],
+    checkpointSaver: memorySaver
 })
 
 // const result = await agent.invoke({
