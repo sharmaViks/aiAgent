2cd279d55e8fec52caaa0c565b6639a2692d77a5
sharmaViks <vikrantsharma725@gmail.com>
2025-04-18 15:11:54 +0530

    agent tools and memory

--- server/agent/agent.js
+++ server/agent/agent.js
@@ -1,23 +1,58 @@
 import {createReactAgent} from "@langchain/langgraph/prebuilt";
 import {ChatAnthropic} from "@langchain/anthropic";
+import {z} from "zod";
+import { tool } from "@langchain/core/tools";
+import { MemorySaver } from "@langchain/langgraph";
+
+const weatherTool = tool(async ({query}) => {
+    console.log('query', query);
+    return 'Weather is sunny';
+},
+{
+    name: "weather",
+    description: "Get the weather in a specific location",
+    schema: z.object({
+        query: z.string().describe("The query to use in search"),
+    }),
+});
 
 const model = new ChatAnthropic({
-    model: "claude-3-5-sonnet-20240620",
+    model: "claude-3-5-sonnet-latest",
     anthropicApiKey: process.env.ANTHROPIC_API_KEY,
 });
 
+const memorySaver = new MemorySaver();
+
 const agent = createReactAgent({
     llm: model,
-    tools: [] 
+    tools: [weatherTool],
+    checkpointSaver: memorySaver 
 })
 
 const result = await agent.invoke({
     messages: [
         {
             role: "user",
-            content: "Hello, how are you?"
+            content: "What's the weather in Delhi?"
         }
     ]
+},
+{
+    configurable:{ thread_id: 42 }
 })
 
 console.log(result.messages.at(-1)?.content);
+
+const followup = await agent.invoke({
+    messages: [
+        {
+            role: "user",
+            content: "What was the city of the user?"
+        }
+    ]
+},
+{
+    configurable:{ thread_id: 42 }
+})
+
+console.log(followup.messages.at(-1)?.content);
